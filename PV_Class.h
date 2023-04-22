bcb11539a82ebc342cbbdd8a8af6ae961e45546c
ConradWarren <130634784+ConradWarren@users.noreply.github.com>
2023-04-22 00:20:31 -0600

    Create PV_Class.h
    Class definition for the PV_Class, or Principle Variation class. This class allows for more advanced move ordering and iterative deepening.

---
+++ PV_Class.h
@@ -0,0 +1,14 @@
+#pragma once
+#define max_ply 64
+
+class PV {
+public:
+	PV();
+	int ply;
+	int killer_moves[2][max_ply];
+	int history_moves[12][64];
+	int pv_length[max_ply];
+	int pv_table[max_ply][max_ply];
+	bool follow_pv_flag;
+	bool score_pv_flag;
+};
