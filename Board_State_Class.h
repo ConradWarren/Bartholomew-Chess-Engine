47bdb2f42134c9345c7e9cf64dd86e02f47b5f39
ConradWarren <130634784+ConradWarren@users.noreply.github.com>
2023-05-12 14:15:01 -0600

    Update Board_State_Class.h
    Added Zobrist Hashing functionality for future transposition table.

--- Board_State_Class.h
+++ Board_State_Class.h
@@ -3,6 +3,11 @@
 
 #define U64 unsigned long long
 
+extern U64 piece_keys[12][64];
+extern U64 en_passant_keys[64];
+extern U64 castle_keys[16];
+extern U64 side_key;
+
 class Board_State {
 
 public:
@@ -13,16 +18,22 @@ public:
 	U64 Bitboards[12];
 	U64 Occupancies[3];
 
+	U64 position_key;
+
 	Board_State();
 	Board_State(std::string Fen);
 	~Board_State();
+	U64 Generate_Zobrist_Key();
 private:
 
 
 };
 
 class moves {
+
 public:
 	int moves[256];
 	int count;
 };
+
+void Init_Zobrist_Keys();
