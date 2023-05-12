51827d58eb61515a46cdc3f34de738d7848a9771
ConradWarren <130634784+ConradWarren@users.noreply.github.com>
2023-05-12 14:19:43 -0600

    Update Main.cpp
    Zobrist Hashing functionality added for future transposition table

--- Main.cpp
+++ Main.cpp
@@ -6,13 +6,16 @@
 #include "Search.h"
 #include "UCI_Interface.h"
 
+
 int main(void) {
 
 	Init_Pre_Calculation();
 
-	Board_State Board = Board_State();
+	Init_Zobrist_Keys();
 
-	UCI_Loop(Board);
+	Board_State board = Board_State();
+
+	UCI_Loop(board);
 
 	return 0; 
 }
