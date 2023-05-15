632ec9cee0dd9ec78057e54144ddb38413e9e235
ConradWarren <130634784+ConradWarren@users.noreply.github.com>
2023-05-14 22:40:34 -0600

    Create Transposition_Table_Class.cpp

---
+++ Transposition_Table_Class.cpp
@@ -0,0 +1,14 @@
+#include "Transposition_Table_Class.h"
+
+#define U64 unsigned long long
+
+transpostion_table::transpostion_table() {
+	key = 0ULL;
+	depth = 0;
+	flag = 0;
+	value = 0;
+}
+
+transpostion_table::~transpostion_table() {
+
+}
